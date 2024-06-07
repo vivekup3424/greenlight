b070eea626fae540f9e68a4ccb81d70ae7b2f48f
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-08 05:17:47 +0530

    add the CRUD ops for movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"` //unique integer id for each movie
@@ -10,4 +13,23 @@ type Movie struct {
 	Runtime   int32     `json:"runtime,omitempty"` //length(runtime) of movie
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
