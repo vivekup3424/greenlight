9498db26de8a306889817a20ea5249e4f1ec328b
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-07 10:16:16 +0530

    make changes in healthcheck:

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -7,10 +7,10 @@ import (
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	s := fmt.Sprintf("Showing details of movie : %s", id)
+	s := fmt.Sprintf("Showing details of movie : %s\n", id)
 	w.Write([]byte(s))
 }
 
 func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("POST request has been acknowledged"))
+	w.Write([]byte("POST request has been acknowledged\n"))
 }
--- internal/routes.go
+++ internal/routes.go
@@ -1,9 +1,1 @@
 package main
-
-import "net/http"
-
-func (app *application) routes() *http.ServeMux{
-    router := http.NewServeMux()
-    return router
-}
-
