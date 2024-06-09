abe74fe206248fa3884bb5ea3b7091cf35aac956
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-09 14:19:38 +0530

    change method from put to patch

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -79,7 +79,7 @@ func main() {
 	router.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 	router.HandleFunc("POST /v1/movies", app.createMoviesHandler)
 	router.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
-	router.HandleFunc("PUT /v1/movies/{id}", app.updateMovieHandler)
+	router.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
 	router.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
 
 	//declare a http with some good timeout settings. which listens
